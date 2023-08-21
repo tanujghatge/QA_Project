79353ab24bdfea544e75ad14737a74c508f6349b
tanujghatge <ghatgetanuj@gmail.com>
2023-08-21 23:55:46 +0530

    Implemented Faiss Vector space

--- app.py
+++ app.py
@@ -4,27 +4,28 @@ from langchain.chat_models import ChatOpenAI
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.prompts import PromptTemplate
+from langchain.callbacks import get_openai_callback
 
 import wandb
 
 from dotenv import load_dotenv
 import os
+import faiss
 
-# def get_embeddings(artifact_name : str):
+def get_embeddings(artifact_name : str):    
+    transcript_embeddings_artifacts = wandb.use_artifact(artifact_name,"dataset")
+    transcript_embeddings_artifacts_dir =  transcript_embeddings_artifacts.download(config.root_artifact_dir)
+    # artifact = wandb.use_artifact('ghatgetanuj/Youtube_playlist_QA/Embeddings_Artifacts:v0', type='dataset')
+    # artifact_dir = artifact.download(config.root_artifact_dir)
     
-#     transcript_embeddings_artifacts = wandb.use_artifact(artifact_name,"dataset")
-#     transcript_embeddings_artifacts_dir =  transcript_embeddings_artifacts.download(config.root_artifact_dir)
+# wandb.init(project=config.project_name, job_type="app")
 
-    
-wandb.init(project=config.project_name, job_type="app")
-
-transcript_embeddings_artifact = wandb.use_artifact(config.transcription_embeddings_artifact, "dataset")
-transcript_embeddings_artifact.download(config.root_artifact_dir / "FAISS")
+# transcript_embeddings_artifact = wandb.use_artifact(config.transcription_embeddings_artifact, "dataset")
+# transcript_embeddings_artifact.download(config.root_artifact_dir / "FAISS")
 # print(faiss_db_dir)
 
 
-load_dotenv()
-OPENAI_API_KEY = os.environ['OPENAI_API_KEY']
+
 
 def get_answer(question : str):
     prompt_template = """Use the following pieces of context to answer the question. 
@@ -41,19 +42,31 @@ def get_answer(question : str):
 
     embeddings = OpenAIEmbeddings(openai_api_key = OPENAI_API_KEY)
     prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
-    faiss_db = FAISS.load_local(r"C:\Drive data\projectss\QA_PROJECT\Downloaded_artifacts\YT_QA_EMB.faiss" , embeddings)
-    return faiss_db
+    # index = faiss.read_index(r"C:\Drive data\projectss\QA_PROJECT\Downloaded_artifacts\YT_QA_EMB.faiss")
+    # faiss_db = FAISS.load_local(r"C:\Drive data\projectss\QA_PROJECT\Downloaded_artifacts", embeddings, index_name="YT_QA_EMB")
+    faiss_db = FAISS.load_local(config.root_artifact_dir, embeddings, index_name="YT_QA_EMB")
+
     # retriever = faiss_db.as_retriever()
-    # retriever.search_kwargs["k"] = 2
 
-    # qa = RetrievalQA.from_chain_type(
-    #     llm=ChatOpenAI(temperature=0),
-    #     chain_type="stuff",
-    #     retriever=retriever,
-    #     chain_type_kwargs={"prompt": prompt},
-    #     return_source_documents=True,
-    # )
+    qa = RetrievalQA.from_chain_type(
+        llm=ChatOpenAI(temperature=0),
+        chain_type="stuff",
+        retriever=faiss_db.as_retriever(),
+        chain_type_kwargs={"k" : 2, "prompt": prompt},
+        return_source_documents=True,
+    )
+
+    with get_openai_callback() as cb:
+        result = qa({"query" : question})
+        print(cb)
+
+    answer = result["result"]
+    return answer
 
 
-db = get_answer("Hello")
-print(db)
+
+if __name__ == "__main__":
+    load_dotenv()
+    wandb.init(project=config.project_name, job_type="app")
+    get_embeddings(config.transcription_embeddings_artifact)
+    OPENAI_API_KEY = os.environ['OPENAI_API_KEY']
